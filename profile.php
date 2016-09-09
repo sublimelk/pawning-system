76b5e679c74a1bb5aa2298c1d56cc2ab4ff19a92
sandun <sandun@sublime.lk>
2016-09-09 12:17:54 +0530

    create user edit form in profile page

--- profile.php
+++ profile.php
@@ -1,3 +1,25 @@
 <?php
+include './includes.php';
 
+if (isset($_POST['btnLogin'])) {
+    
+    $result = User::editUser($_POST);
+}
+?>
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="POST">
+            <input name="name" type="text" value="<?php echo $_SESSION['name']; ?>" />
+            <input name="username" type="text" value="<?php echo $_SESSION['username']; ?>"/>
+            <input name="password" type="password"  value="<?php echo $_SESSION['password']; ?>"/>
+            <input name="btnLogin" type="submit"  value="Login"/>
+        </form>
+
+    </body>
+</html>
 
