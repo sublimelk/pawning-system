bdf990a8b3e80eb598d226c4764f455fb3b8a10e
sandun <sandun@sublime.lk>
2016-09-09 09:15:39 +0530

    Create New User class and add main options

---
+++ class/User.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author official
+ */
+class User {
+
+    public function setUser($data) {
+        
+    }
+
+    public function getUser() {
+        
+    }
+
+    public function isLoginUser() {
+        
+    }
+
+}
