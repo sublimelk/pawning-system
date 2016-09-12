6de0a4d81ac236b279a70548d8399a6c290bf281
sandun <sandun@sublime.lk>
2016-09-12 11:19:20 +0530

    change text field names

--- edit_customer.php
+++ edit_customer.php
@@ -13,13 +13,13 @@ if (isset($_POST['update'])) {
     <label>Name :</label>
     <input type="text" name="name" value="<?php echo $customer['name']; ?>"/> <br>
     <label>ID Number :</label>
-    <input type="text" name="id_num" value="<?php echo $customer['id_num']; ?>"/> <br>
+    <input type="text" name="nic" value="<?php echo $customer['nic']; ?>"/> <br>
     <label>Address :</label>
     <input type="text" name="address" value="<?php echo $customer['address']; ?>"/> <br>
     <label>Phone :</label>
     <input type="text" name="phone" value="<?php echo $customer['phone']; ?>"/> <br>
     <label>Value :</label>
-    <input type="text" name="value" value="<?php echo $customer['value']; ?>"/> <br>
+    <input type="text" name="feedback" value="<?php echo $customer['feedback']; ?>"/> <br>
     
     <input type="submit" name="update" value="Submit"/>
     
