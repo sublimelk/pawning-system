f1edbff8ae11f7e6963aaad90123e662dcaf7733
sandun <sandun@sublime.lk>
2016-09-12 11:50:23 +0530

    create SystemData class file

---
+++ class/SystemData.php
@@ -0,0 +1,16 @@
+<?php
+
+class SystemData {
+
+    public function getItemTypes() {
+        $data = array(
+            '1' => 'Type 01',
+            '2' => 'Type 02',
+            '3' => 'Type 03',
+            '4' => 'Type 04'
+        );
+
+        return $data;
+    }
+
+}
