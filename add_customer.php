0fd624a33fc1eab18afdbb77c80c5f0bbcf21663
sandun <sandun@sublime.lk>
2016-09-12 11:52:30 +0530

    create select box

--- add_customer.php
+++ add_customer.php
@@ -9,16 +9,28 @@ if (isset($_POST['save'])) {
 <form method="post">
     <label>Name :</label>
     <input type="text" name="name" placeholder="Name"/> <br>
-    <label>ID Number :</label>
-    <input type="text" name="id_num" placeholder="ID"/> <br>
+    <label>NIC Number :</label>
+    <input type="text" name="nic" placeholder="ID"/> <br>
     <label>Address :</label>
     <input type="text" name="address" placeholder="Address"/> <br>
     <label>Phone :</label>
     <input type="text" name="phone" placeholder="phone"/> <br>
-    <label>Value :</label>
-    <input type="text" name="value" placeholder="Value"/> <br>
+    <label>Feedback :</label>
+    <input type="text" name="feedback" placeholder="fedback"/> <br>
 
     <input type="submit" name="save" value="Submit"/>
-    
+
+    <select>
+        <option value=""> --- Please Select --- </option>
+        <?php
+        foreach (SystemData::getItemTypes() as $key => $type) {
+             
+            ?>
+
+        <option value="<?php echo $key; ?>"> <?php echo $type; ?></option>
+            <?php
+        }
+        ?>
+    </select>
 </form>
 
