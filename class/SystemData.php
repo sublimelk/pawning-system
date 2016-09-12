cee481c4fc0f45a074c23e7da4b7ab0542813145
sandun <sandun@sublime.lk>
2016-09-12 13:02:01 +0530

    add function to get carat size

--- class/SystemData.php
+++ class/SystemData.php
@@ -13,4 +13,32 @@ class SystemData {
         return $data;
     }
 
+    public function getCaratSize() {
+        $size = array(
+            '1' => 'Carat 18',
+            '2' => 'Carat 22',
+        );
+        return $size;
+    }
+
+    public function getCustomers() {
+        $db = new DB();
+
+        $sql = "SELECT * FROM customer WHERE `is_active` = '1'";
+
+        $result = $db->readQuery($sql);
+
+        $array_res = array();
+
+        while ($row = mysql_fetch_array($result)) {
+
+            $cus = array(
+                'name' => $row['name'],
+            );
+
+            array_push($array_res, $cus);
+        }
+        return $array_res;
+    }
+
 }
