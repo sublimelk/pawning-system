d761b4f00b8306f7ebd2e49a62363ce7021c6c0d
sandun <sandun@sublime.lk>
2016-09-12 11:51:27 +0530

    change name in view_customer.php

--- navigation.php
+++ navigation.php
@@ -1,13 +1,29 @@
-<?php 
-    $user = User::getUser();
-    $customer = Customer::getCustomers();
+<?php
+$user = User::getUser();
+$customer = Customer::getCustomers();
 ?>
 <ul> 
     <li>
-        <a href="add_customer.php">Customer</a>
+        <a>Customers</a>
+        <ul> 
+            <li>
+                <a href="add_customer.php">Add New</a>
+            </li>
+            <li>
+                <a href="view_customers.php">View</a>
+            </li>
+        </ul>
     </li>
     <li>
-        <a href="view_customer.php">View Customers</a>
+        <a>Pawning</a>
+        <ul> 
+            <li>
+                <a href="add_pawning.php">Add New</a>
+            </li>
+            <li>
+                <a href="view_pawning.php">View</a>
+            </li>
+        </ul>
     </li>
     <li>
         <a href="profile.php?id=<?php echo $user['id']; ?>">Profile</a>
