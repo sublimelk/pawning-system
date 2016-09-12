330686020768ba55b3072a209348b9195e4184e6
sandun <sandun@sublime.lk>
2016-09-12 15:12:10 +0530

    create table and show deatail in the table

--- view_pawning.php
+++ view_pawning.php
@@ -1,8 +1,33 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    include './includes.php';
+    
+  $details = SystemData::getPawning();
+?>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 15px;
+}
+</style>
+<table>
+    <tr>
+    <th>Name</th>
+    <th>Edit</th>
+    <th>Delete</th>
+    </tr>
+    <?php
+    foreach ($details as $detail){
+    ?>
+    <tr>
+        <td><?php echo $detail['cus_name']; ?></td>
+        <td><a href="edit_pawning.php?id=<?php echo $detail['id']; ?>">Edit</a></td>
+        <td><a href="delete_pawning.php?id=<?php echo $detail['id']; ?>">Delete</a></td>
+        <?php 
+    }
+        ?>
+    </tr>
+</table>
 
