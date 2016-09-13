1e1733a67f4423c9c9a0c6390b0dddef51d73c74
sandun <sandun@sublime.lk>
2016-09-13 16:40:48 +0530

    add new function to upload customer photos

--- class/Customer.php
+++ class/Customer.php
@@ -77,10 +77,102 @@ class Customer {
         $db = new DB();
 
         $sql = "UPDATE `customer` SET `is_active` = '0' WHERE `id` = $id ";
-        
+
         $result = $db->readQuery($sql);
 
         return $result;
+    }
+
+    public function setPhoto($customerId, $file) {
+
+        $target_dir = "img/customers/";
+
+        $ext = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+
+        $filepath = $target_dir . $customerId . '.' . $ext;
+
+        $uploadOk = 1;
+
+// Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($file["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+// Check file size
+        if ($file["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+// Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($file["fileToUpload"]["tmp_name"], $filepath)) {
+ 
+                $resizeObj = new ImageResizer($filepath);
+                $resizeObj->resizeImage(500, 500, 'crop');
+                $resizeObj->saveImage($filepath, 80);
+ 
+                echo "The file " . basename($file["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+    
+    public function editPhoto($customerId, $file) {
+
+        $target_dir = "img/customers/";
+
+        $ext = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+
+        $filepath = $target_dir . $customerId . '.' . $ext;
+
+        $uploadOk = 1;
+
+// Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($file["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+// Check file size
+        if ($file["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+// Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($file["fileToUpload"]["tmp_name"], $filepath)) {
+ 
+                $resizeObj = new ImageResizer($filepath);
+                $resizeObj->resizeImage(500, 500, 'crop');
+                $resizeObj->saveImage($filepath, 80);
+ 
+                echo "The file " . basename($file["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
     }
 
 }
