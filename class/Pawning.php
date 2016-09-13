9a989c26e81b480b5cc6ebcfe13210a7711c668e
sandun <sandun@sublime.lk>
2016-09-13 16:36:19 +0530

    change sql table column name

--- class/Pawning.php
+++ class/Pawning.php
@@ -5,7 +5,7 @@ class Pawning {
 
         $db = new DB();
 
-        $sql = "INSERT INTO `pawning` (`date`,`cus_name`,`item_type`,`car_size`,`weight`,`interest`,`amount`) VALUES ('" . $_POST['date'] . "','" . $_POST['cus_name'] . "','" . $_POST['item_type'] . "','" . $_POST['car_size'] . "','" . $_POST['weight'] . "','" . $_POST['interest'] . "','" . $_POST['amount'] . "')";
+        $sql = "INSERT INTO `pawning` (`date`,`customer`,`item_type`,`car_size`,`weight`,`interest`,`amount`) VALUES ('" . $_POST['date'] . "','" . $_POST['customer'] . "','" . $_POST['item_type'] . "','" . $_POST['car_size'] . "','" . $_POST['weight'] . "','" . $_POST['interest'] . "','" . $_POST['amount'] . "')";
 
         $result = $db->readQuery($sql);
 
@@ -26,7 +26,7 @@ class Pawning {
             $pawning = array(
                 'id' => $row['id'],
                 'date' => $row['date'],
-                'cus_name' => $row['cus_name'],
+                'customer' => $row['customer'],
                 'item_type' => $row['item_type'],
                 'car_size' => $row['car_size'],
                 'weight' => $row['weight'],
@@ -42,7 +42,7 @@ class Pawning {
     public function editPawning($id) {
         $db = new DB();
 
-        $sql = "UPDATE `pawning` SET `date` = '" . $_POST['date'] . "', `cus_name` = '" . $_POST['cus_name'] . "', `item_type` = '" . $_POST['item_type'] . "', `car_size` = '" . $_POST['car_size'] . "' , `weight` = '" . $_POST['weight'] . "' , `amount` = '" . $_POST['amount'] . "' WHERE id = $id ";
+        $sql = "UPDATE `pawning` SET `date` = '" . $_POST['date'] . "', `item_type` = '" . $_POST['item_type'] . "', `car_size` = '" . $_POST['car_size'] . "' , `weight` = '" . $_POST['weight'] . "' , `amount` = '" . $_POST['amount'] . "' WHERE id = $id ";
 
         $result = $db->readQuery($sql);
 
