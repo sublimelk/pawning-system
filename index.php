d8ecc0fe2357098f364ffc64f5ce2422c7079c88
sandun <sandun@sublime.lk>
2016-09-14 13:34:15 +0530

    link css files

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ if (!User::isLoginUser()) {
     <head>
         <meta charset="UTF-8">
         <title>Dashbord</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php include './navigation.php'; ?>
