a4b77baf9bb5f4bf1ad1d86af56418b7f57826c8
sandun <sandun@sublime.lk>
2016-09-20 17:01:48 +0530

    include time zone

--- includes.php
+++ includes.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+date_default_timezone_set("Asia/Calcutta");
+ 
 include './class/DB.php';
 include './class/User.php';
 include './class/Customer.php';
@@ -8,4 +10,5 @@ include './class/SystemData.php';
 include '/class/Pawning.php';
 include './class/ImageResizer.php';
 include './class/Releasing.php';
-
+include './class/Report.php';
+ 
