c2d0aecac1c7a508d23e82c4da11050f7730213b
sandun <sandun@sublime.lk>
2016-09-20 17:02:12 +0530

    delete comments

--- class/Customer.php
+++ class/Customer.php
@@ -93,7 +93,6 @@ class Customer {
 
         $uploadOk = 1;
 
-// Check if image file is a actual image or fake image
         if (isset($_POST["submit"])) {
             $check = getimagesize($file["fileToUpload"]["tmp_name"]);
             if ($check !== false) {
@@ -105,16 +104,14 @@ class Customer {
             }
         }
 
-// Check file size
         if ($file["fileToUpload"]["size"] > 500000) {
             echo "Sorry, your file is too large.";
             $uploadOk = 0;
         }
 
-// Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+
         } else {
             if (move_uploaded_file($file["fileToUpload"]["tmp_name"], $filepath)) {
 
