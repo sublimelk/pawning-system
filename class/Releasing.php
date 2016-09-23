c7a15f49f5150ad990f5de33dcbdf60ed9aa683e
sandun <sandun@sublime.lk>
2016-09-23 15:44:55 +0530

    change input filed name

--- class/Releasing.php
+++ class/Releasing.php
@@ -6,7 +6,7 @@ class Releasing {
 
         $db = new DB();
 
-        $sql = "INSERT INTO `releasing` (`date`,`pawning`,`releasing_interest`,`settle_amount`) VALUES ('" . $_POST['date'] . "', '" . $_POST['pawning_id'] . "', '" . $_POST['interest'] . "', '" . $_POST['setl_amount'] . "')";
+        $sql = "INSERT INTO `releasing` (`date`,`pawning`,`releasing_interest`,`settle_amount`) VALUES ('" . $_POST['day_to'] . "', '" . $_POST['pawning_id'] . "', '" . $_POST['interest'] . "', '" . $_POST['setl_amount'] . "')";
 
         $result = $db->readQuery($sql);
 
