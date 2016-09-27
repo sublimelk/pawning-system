68f8c254e4cc66d6af8e40bff19f35a662573e1b
sandun <sandun@sublime.lk>
2016-09-27 16:46:17 +0530

    add jquery function to claculate value

--- edit_releasing.php
+++ edit_releasing.php
@@ -5,13 +5,10 @@ $message = NULL;
 
 $id = $_GET['id'];
 
-$detail = Releasing::getAllById($id);
-
-
 if (isset($_POST['update'])) {
 
     $res = Releasing::editreleasing($id);
-    
+
     if ($res) {
         $message = ' You successfully update Release  ';
     } else {
@@ -19,10 +16,11 @@ if (isset($_POST['update'])) {
     }
 
     $customerId = $_GET['id'];
-    
 }
 
 $detail = Releasing::getAllById($id);
+
+$pawning = Pawning::getPawningById($detail['pawning']);
 ?>
 <html>
     <head>
@@ -43,7 +41,37 @@ $detail = Releasing::getAllById($id);
                 $('#datetimepicker1').datetimepicker({
                     language: 'pt-BR',
                 });
+
+                $('#interest, #day_to').on('keyup change select', function () {
+
+                    var day1 = new Date($("#day_from").val());
+                    var day2 = new Date($("#day_to").val());
+                    var mon = monthDiff(day1, day2);
+                    var int = ($("#interest").val());
+                    var amn = ($("#value").val());
+
+                    var amount = ((mon * amn * int / 100))
+
+                    $('#int_amount').val(amount);
+
+                    var set_amount = parseFloat(amount) + parseFloat(amn);
+                    $('#amount').val(set_amount);
+
+                });
             });
+            function monthDiff(d1, d2) {
+                var months;
+                months = (d2.getFullYear() - d1.getFullYear()) * 12;
+                months -= d1.getMonth();
+                months += d2.getMonth();
+
+                if (months == 0) {
+                    months = (d2.getDay() - d1.getDay());
+                }
+
+                return months <= 0 ? 0 : months;
+            }
+
         </script>
     </head>
     <body>
@@ -52,8 +80,8 @@ $detail = Releasing::getAllById($id);
             <?php
             if ($message) {
                 ?>
-                <div class="alert alert-dismissible" role="alert">
-                    <a href="#" class="alert-link"><?php echo $message;?></a>
+                <div class="alert alert-info" role="alert">
+                    <a href="#" class="alert-link"><?php echo $message; ?></a>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -69,11 +97,18 @@ $detail = Releasing::getAllById($id);
                     <div class="row">
                         <div class="col-sm-9">
                             <form action="" method="POST" enctype="multipart/form-data" class="form-horizontal">
+
+                                <input type="hidden" name="day_from" id="day_from" value="<?php echo $pawning['date'];?>"/>
+                                
+                                <input type="hidden" name="value" id="value" value="<?php echo $pawning['amount']; ?>"/>
+                                
+                                <input type="hidden" name="int_amount" id="int_amount"/>
+
                                 <div class="form-group">
                                     <label for="date" class="col-sm-3 control-label">Date</label>
                                     <div class="col-sm-9">
                                         <div id="datetimepicker1" class="input-append date"> 
-                                            <input data-format="yyyy-MM-dd hh:mm:ss" name="date" class="form-control date_picker" value="<?php echo $detail['date']; ?>"/> 
+                                            <input data-format="yyyy-MM-dd hh:mm:ss" name="day_to" id="day_to" class="form-control date_picker" value="<?php echo $detail['date']; ?>"/> 
                                             <span class="add-on">
                                                 <i class="glyphicon glyphicon-calendar" ></i>
                                             </span>
@@ -82,16 +117,16 @@ $detail = Releasing::getAllById($id);
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="amount" class="col-sm-3 control-label">Settle Amount</label>
+                                    <label for="interest" class="col-sm-3 control-label">Interest(%)</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="amount" id="amount" class="form-control" value="<?php echo $detail['settle_amount']; ?>" autocomplete="off"/> 
+                                        <input type="text" name="interest" id="interest" class="form-control" value="<?php echo $detail['releasing_interest']; ?>" autocomplete="off"/>
                                     </div>
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="interest" class="col-sm-3 control-label">Interest(%)</label>
+                                    <label for="amount" class="col-sm-3 control-label">Settle Amount</label>
                                     <div class="col-sm-9">
-                                        <input type="text" name="interest" id="nterest" class="form-control" value="<?php echo $detail['releasing_interest']; ?>" autocomplete="off"/>
+                                        <input type="text" name="amount" id="amount" class="form-control" value="<?php echo $detail['settle_amount']; ?>" autocomplete="off"/> 
                                     </div>
                                 </div>
 
