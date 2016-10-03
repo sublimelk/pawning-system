6bf47c9f3bfd2e276b9103e8ce94ec351d6f4637
sandun <sandun@sublime.lk>
2016-10-03 14:53:47 +0530

    add validation part in to login page

--- login.php
+++ login.php
@@ -4,10 +4,15 @@ session_start();
 include './class/DB.php';
 include './class/User.php';
 
-if (isset($_POST['btnLogin'])) {
-    
-$result = User::loginUser($_POST);
+$message = NULL;
 
+if (isset($_POST['btnLogin'])) {
+
+    $result = User::loginUser($_POST);
+    
+    if ($result == FALSE) {
+        $message = 'Invalid Username or Password !';
+    } 
 }
 ?>
 <!DOCTYPE html> 
@@ -19,13 +24,25 @@ $result = User::loginUser($_POST);
         <link href="css/navbar.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" type="text/css" media="all" href="css/color-theme.css" />
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
-        
+
         <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
         <script src="js/togelmenu.js" type="text/javascript"></script>
     </head>
     <body>
-        <div class="container">
+        <div class="container-fluid">
+            <?php
+            if ($message) {
+                ?>
+                <div class="alert alert-danger" role="alert" id="alertt">
+                    <a href="#" class="alert-link"><?php echo $message; ?></a>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php
+            }
+            ?> 
             <div class="row vertical-offset-100">
                 <div class="col-md-4 col-md-offset-4">
                     <div class="panel panel-default">
@@ -40,8 +57,8 @@ $result = User::loginUser($_POST);
                                     <label class="panel-login">
                                         <div class="login_result"></div>
                                     </label>
-                                    <input class="form-control" placeholder="Username" name="username" id="username" type="text">
-                                    <input class="form-control" placeholder="Password" name="password" id="password" type="password">
+                                    <input class="form-control" placeholder="Username" name="username" id="username" type="text" autocomplete="off">
+                                    <input class="form-control" placeholder="Password" name="password" id="password" type="password" autocomplete="off">
                                     <br></br>
                                     <input class="btn btn-lg btn-success btn-block" type="submit" name="btnLogin" id="login" value="Login »">
                                 </fieldset>
